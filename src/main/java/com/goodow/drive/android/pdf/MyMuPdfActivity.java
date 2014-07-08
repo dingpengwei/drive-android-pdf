b759c2b65fa4658c4eba84044b2e84180474413f
dingpengwei <dingpengwei@goodow.com>
2014-07-08 17:09:17 +0800

    rmMu

--- src/main/java/com/goodow/drive/android/pdf/MyMuPdfActivity.java
+++ src/main/java/com/goodow/drive/android/pdf/MyMuPdfActivity.java
@@ -1,13 +1,13 @@
 package com.goodow.drive.android.pdf;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import com.artifex.mupdfdemo.MuPDFActivity;
 
 /**
  * Created by dpw on 7/8/14.
  */
-public class MyMuPdfActivity extends MuPDFActivity{
+public class MyMuPdfActivity extends Activity{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -16,7 +16,6 @@ public class MyMuPdfActivity extends MuPDFActivity{
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
-        menu.clear();
         return false;
     }
 
